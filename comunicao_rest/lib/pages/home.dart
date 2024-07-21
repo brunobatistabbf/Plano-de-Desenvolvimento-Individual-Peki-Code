e0bf99a6d49b8f062475f1ab1466d4b5aeae7e77
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2024-07-21 18:31:39 -0300

    updating

--- comunicao_rest/lib/pages/home.dart
+++ comunicao_rest/lib/pages/home.dart
@@ -1,7 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../services/api_service.dart';
+import '../models/posts.dart';
+import 'comments.dart';
+import 'login.dart';
 
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
+  final _apiService = ApiService();
+  int? _userId;
+  String? _username;
+  List<Post> _posts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUser();
+  }
+
+  Future<void> _loadUser() async{
+    final prefs = await SharedPreferences.getInstance();
+    final userId = prefs.getInt('userId');
+    final username = prefs.getString('username');
+
+    if (userId != null && username != null) {
+      setState(() {
+        _userId = userId;
+        _username = username;
+      });
+      _loadPosts(userId);
+    } else {
+      _logout();
+    }
+  }
+
+  Future<void> _loadPosts(int userId) async {
+    final posts = await _apiService.getUserPosts(userId);
+    setState(() {
+      _posts = posts;
+    });
+  }
+
+  void _logout() async{
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+    Navigator.pushReplacement(context, 
+    MaterialPageRoute(builder: (context) => LoginPage())
+    );
+  }
+
+
+@override
+Widget build(BuildContext context){
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Posts de $_username'),
+      actions: [
+        IconButton(onPressed: _logout, icon: Icon(Icons.logout))
+      ],
+    ),
+    body: ListView.builder(
+      itemCount: _posts.length,
+      itemBuilder: (context, index){
+        final post = _posts[index];
+        return ListTile(
+          title: Text(post.title),
+          subtitle: Text(post.body),
+          onTap: (){
+            Navigator.push(
+              context, 
+              MaterialPageRoute(
+                builder: (context) => CommentsPage(postId: post.id),
+                ),
+              );
+          }
+        );
+      }
+      ),
+  );
+}
+
+
+
+
 }
--- comunicao_rest/lib/pages/login.dart
+++ comunicao_rest/lib/pages/login.dart
@@ -1,3 +1,64 @@
+import 'package:comunicao_rest/services/api_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _usernameController =  TextEditingController();
+  final _apiService = ApiService();
+  bool _isLoading = false;
+
+
+  Future<void> _login() async{
+    setState(() {
+      _isLoading = true;
+    });
+  
+
+    final username = _usernameController.text;
+    final user = await _apiService.login(username);
+
+    setState(() {
+      _isLoading = false;
+    });
+
+    if (user != null){
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setInt('userID', user.id);
+      await prefs.setString('username', user.username);
+
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage() )); 
+    } else{
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Usuário Inválido!")));
+    };
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            SizedBox(height: 20),
+            _isLoading
+              ? CircularProgressIndicator() : ElevatedButton(onPressed: _login, child: Text('Login'),
+              ),
+          ],
+        ),
+        
+        ),
+    );
+  }
+}
